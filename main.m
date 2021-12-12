bac4be50f965ac9b8037e25ce1abb2a23527f5b1
evyatar-bur <65293193+evyatar-bur@users.noreply.github.com>
2021-12-12 14:49:52 +0200

    Update main.m

--- main.m
+++ main.m
@@ -6,3 +6,39 @@ clc
 %% Read data
 
 [num, txt, raw] = xlsread('our_data\360.xlsx');
+
+
+%% Extract features
+
+
+
+
+%% Normalize data
+
+
+
+
+%% Split to train and Test
+
+
+
+
+%% Feature selection
+
+
+
+
+%% Create model Train/Test
+
+
+
+
+%% Show results
+
+
+
+
+%% Create final model from all of the data
+
+
+
