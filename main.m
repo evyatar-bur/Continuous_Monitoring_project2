4684bb928799695547772d483e18b89b150e3318
evyatar-bur <65293193+evyatar-bur@users.noreply.github.com>
2021-12-27 13:31:52 +0200

    Update main.m

--- main.m
+++ main.m
@@ -3,9 +3,41 @@ close all
 clear
 clc
 
+addpath('C:\dev\Continuous_Monitoring_project2\Data BHQ\')
+
+% Read recordings
+d=dir('*.label.xlsx');
+
+X_event=zeros(50000,10)-99;    % Allocate memory for matrix X, with default value -99
+Y_event=zeros(50000,1)-99;     % Allocate memory for label vector Y
+
 %% Read data
 
-[num, txt, raw] = xlsread('our_data\360.xlsx');
+for r=1:length(d)
+
+    % Read data from recordings
+    recording = strrep(d(r).name,'.label','');
+
+    [num, txt, raw] = xlsread(recording);
+
+
+
+
+
+    B=readtable(gyro_file);
+    label_file=strrep(d(r).name,'Acc','Label');
+    C=readtable(label_file);
+    acc_x=A.x_axis_g_;
+    acc_y=A.y_axis_g_;
+    acc_z=A.z_axis_g_;
+    gyro_x=B.x_axis_deg_s_;
+    gyro_y=B.y_axis_deg_s_;
+    gyro_z=B.z_axis_deg_s_;
+
+
+
+
+
 
 
 %% Extract features
