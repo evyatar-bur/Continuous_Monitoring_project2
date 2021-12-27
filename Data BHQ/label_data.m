57b484056d90cbb9331ab83c9f6d24ab6ec9f681
yuvalkeret <yuvalker@gmail.com>
2021-12-27 15:37:26 +0200

    label data

---
+++ Data BHQ/label_data.m
@@ -0,0 +1,15 @@
+function [Time,Sleep, WakeUp,Feeling,Activities] = label_data(recording)
+    [~,~,data]=xlsread(recording);
+   
+    if strcmp(recording,'340.label.xlsx')
+    data = data(1:50,:);
+    end
+   
+     
+    Time=data(2:end,1);
+    Sleep=data(2:end,2);
+    WakeUp=data(2:end,3);
+    Feeling=data(2:end,4);
+    Activities=data(2:end,5);
+
+end
